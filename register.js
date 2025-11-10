2af5fc4d59fb14eb6a3a89d50fcadaedb8ff2210
kerimKerimKerimKerimkerim <kerimkeirm7@gmail.com>
2025-11-10 14:03:51 +0300

    Update register.js

--- register.js
+++ register.js
@@ -12,7 +12,7 @@ document.getElementById("registerForm").addEventListener("submit", function(e){
         return;
     }
 
-    fetch("http://127.0.0.1:5000/register", {
+    fetch("/register", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({username, password})
